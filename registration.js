8574b3d12ae743a4aedf16fb5b5a55a470df7899
Wasym Shykh <wasymshykh@gmail.com>
2019-07-10 23:39:54 +0500

    Email notification

--- registration.js
+++ registration.js
@@ -1,14 +1,34 @@
 const rp = require("request-promise");
 const cheerio = require("cheerio");
+const nodemailer = require("nodemailer");
+const dotenv = require("dotenv");
 
-let sic =
+dotenv.config();
+
+// Array with list of courses you want check if they are available
+const wantCourse = ["SOFTWARE PROJECT MANAGEMENT", "SOFTWARE CONSTRUCTION"];
+
+// Change to your data
+const config = {
+    receiver_email: "wasymshykh@gmail.com",
+    sender_name: "Wasym",
+    sender_email: "wasym@opticaldot.com"
+};
+
+const sic =
     "http://iulms.edu.pk/registration/Registration_FEST_student_EarlyRegistration.php";
 
 const send_request = async url => {
+    console.log("Sending Request...");
+
     let html = await rp(url, {
         headers: {
             Cookie:
-                "_ga=GA1.3.1620953679.1551285319; MoodleSession=; MoodleSessionTest=; MOODLEID_=%25B0%259AG%2519%25E5",
+                "_ga=GA1.3.1620953679.1551285319; MoodleSession=" +
+                process.env.MOODLE_SESSION +
+                "; MoodleSessionTest=" +
+                process.env.MOODLE_SESSION_TEST +
+                "; MOODLEID_=%25B0%259AG%2519%25E5",
             Host: "iulms.edu.pk",
             "User-Agent":
                 "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"
@@ -18,60 +38,131 @@ const send_request = async url => {
     return html;
 };
 
-send_request(sic)
-    .then(res => {
-        const $ = cheerio.load(res);
+console.log("Started watching, waiting for interval to send request...");
 
-        let form = $("form").find("table");
-        form = form.next();
-        form = form.next();
-        form = form.next();
-        form = form.next();
+const re_request = setInterval(() => {
+    send_request(sic)
+        .then(res => {
+            const $ = cheerio.load(res);
 
-        while (form.html()) {
-            html_main_table = form.html();
-            const $$ = cheerio.load(html_main_table);
+            let form = $("form").find("table");
 
-            let tr = $$("table").find("tr");
-
-            let isFirst = true;
-
-            while (tr.html()) {
-                let td = tr.find("td");
-
-                let i = 0;
-                let stringToBe = "";
-                let tr_itrate = true;
-                while (td.html()) {
-                    let td_text = td.html().trim();
-
-                    if (i == 0 && td_text.indexOf("<input") == -1) {
-                        tr_itrate = false;
-                    }
-
-                    if (tr_itrate) {
-                        // 3:CH; 4:Name; 6:Teacher; 7:Time; 8:Strength
-                        if (i == 3 || i == 4 || i == 6 || i == 7 || i == 8) {
-                            stringToBe += td_text + "||";
-                        }
-                        td = td.next();
-                        i++;
-                    } else {
-                        break;
-                    }
-                }
-                console.log("STT:::" + stringToBe);
-
-                //console.log(tr.html());
-
-                tr = tr.next();
-                isFirst = false;
+            if (!form) {
+                console.log("Invalid MOODLE_SESSION OR MOODLE_SESSION_TEST");
             }
 
-            form = form.next();
-            break;
-        }
-    })
-    .catch(err => {
-        console.error(err);
-    });
+            const available = [];
+
+            while (form.html()) {
+                html_main_table = form.html();
+                const $$ = cheerio.load(html_main_table);
+
+                let tr = $$("table").find("tr");
+
+                while (tr.html()) {
+                    let td = tr.find("td");
+
+                    let i = 0;
+                    let details = {
+                        credithour: false,
+                        name: false,
+                        teacher: false,
+                        timing: false,
+                        strength: false
+                    };
+                    let tr_itrate = true;
+                    while (td.html()) {
+                        let td_text = td.html().trim();
+
+                        if (i == 0 && td_text.indexOf("<input") == -1) {
+                            tr_itrate = false;
+                        }
+
+                        if (tr_itrate) {
+                            // 3:CH; 4:Name; 6:Teacher; 7:Timing; 8:Strength
+                            if (i == 3) details.credithour = td_text;
+                            if (i == 4) details.name = td_text;
+                            if (i == 6) details.teacher = td_text;
+                            if (i == 7) details.timing = td_text;
+                            if (i == 8) details.strength = td_text;
+
+                            td = td.next();
+                            i++;
+                        } else {
+                            break;
+                        }
+                    }
+                    if (details.name) {
+                        available.push(details);
+                    }
+
+                    tr = tr.next();
+                }
+
+                form = form.next();
+            }
+
+            let result = available.find(course => {
+                return wantCourse.includes(course.name);
+            });
+
+            if (result) {
+                // Sending email!
+
+                const transporter = nodemailer.createTransport({
+                    host: process.env.SMTP_AUTH_HOST,
+                    port: 587,
+                    secure: false,
+                    auth: {
+                        user: process.env.SMTP_AUTH_USER,
+                        pass: process.env.SMTP_AUTH_PASS
+                    },
+                    tls: {
+                        rejectUnauthorized: false
+                    }
+                });
+
+                transporter.verify(function(err, success) {
+                    if (err) {
+                        console.error("SMTP Config Incorrect");
+                        process.exit();
+                    }
+                });
+
+                var message = {
+                    from:
+                        '"' +
+                        config.sender_name +
+                        '" <' +
+                        config.sender_email +
+                        ">",
+                    to: config.receiver_email,
+                    subject: 'Course "' + result.name + '" Khul Gya!',
+                    text: "Jaldi sy iulms check kro jani",
+                    html: "<p>Jaldi sy iulms check kro jani</p>"
+                };
+
+                transporter.sendMail(message, (err, info) => {
+                    if (err) {
+                        console.error(
+                            "Couldn't send email, btw, course \"" +
+                                result.name +
+                                '" is available'
+                        );
+                    } else {
+                        wantCourse.pop(result.name);
+                        console.log(
+                            'Course "' +
+                                result.name +
+                                '" is available, register now!'
+                        );
+                    }
+                });
+            }
+        })
+        .catch(err => {
+            clearInterval(re_request);
+            console.error(err);
+            process.exit();
+        });
+}, 90000);
